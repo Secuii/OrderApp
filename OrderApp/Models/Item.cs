bc46110e7e9327891b6446e7482c78e06f404072
Vic7orSeg <victor.seg.0790@gmail.com>
2024-01-02 20:18:50 +0100

    first commit test
    hice algo y no tengo ni idea lo que hice.

---
+++ OrderApp/Models/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderApp.Models
+{
+    public class Item
+    {
+        public Item(int id, string eng_name, string esp_name)
+        {
+            this.id = id;
+            this.eng_name = eng_name;
+            this.esp_name = esp_name;
+        }
+
+        public int id { get; set; }
+        public string eng_name { get; set; }
+        public string esp_name { get; set; }
+    }
+}
